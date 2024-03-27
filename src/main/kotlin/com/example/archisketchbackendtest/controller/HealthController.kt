eec7aedabaf4a9990131ada7e1af7ac8457c5b69
mck0324 <dch3030@naver.com>
2024-03-27 23:58:36 +0900

    Make health check API(GET)

---
+++ src/main/kotlin/com/example/archisketchbackendtest/controller/HealthController.kt
@@ -0,0 +1,16 @@
+package com.example.archisketchbackendtest.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class HealthController {
+
+    @GetMapping("")
+    fun healthCheck(): String {
+        return "1"
+    }
+
+}
