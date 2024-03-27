aaad9e1bc11b42d106b6ef145bbb56adc705c5a1
mck0324 <dch3030@naver.com>
2024-03-27 23:48:06 +0900

    First Commit

---
+++ src/main/kotlin/com/example/archisketchbackendtest/ArchisketchBackendTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.archisketchbackendtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArchisketchBackendTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArchisketchBackendTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/archisketchbackendtest/ArchisketchBackendTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.archisketchbackendtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArchisketchBackendTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
