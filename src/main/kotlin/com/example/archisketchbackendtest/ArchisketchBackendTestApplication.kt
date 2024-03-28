738cae3ea89ea3d99281d348153fc4ce3b4271e9
mck0324 <dch3030@naver.com>
2024-03-28 23:52:30 +0900

    Make DataStructureAPI - Entity(ApartUnit)

--- src/main/kotlin/com/example/archisketchbackendtest/ArchisketchBackendTestApplication.kt
+++ src/main/kotlin/com/example/archisketchbackendtest/ArchisketchBackendTestApplication.kt
@@ -2,8 +2,10 @@ package com.example.archisketchbackendtest
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class ArchisketchBackendTestApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/example/archisketchbackendtest/model/ApartmentUnit.kt
+++ src/main/kotlin/com/example/archisketchbackendtest/model/ApartmentUnit.kt
@@ -5,23 +5,39 @@ import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.math.BigDecimal
 import java.time.LocalDateTime
 
 @Entity
 @EntityListeners(AuditingEntityListener::class)
-class Apartment (
+class ApartmentUnit (
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
 
-    val name: String? = null,
+    val exclusiveArea: BigDecimal? = null,
+
+    val suppliedArea: BigDecimal? = null,
+
+    val blueprintImage: String? = null,
+
+    val type: String? = null,
+
+    val extensionType: String? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "apartment_id")
+    val apartment: Apartment? = null,
 
     @CreatedDate
     var createdAt: LocalDateTime? = null,
     @LastModifiedDate
     var updatedAt: LocalDateTime? = null,
+
 )
